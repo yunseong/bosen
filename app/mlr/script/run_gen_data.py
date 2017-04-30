38f6792c4fd1fe9889aa9d5d386b48902e85942b
Yunseong Lee <yunseong@apache.org>
2017-04-30 20:53:04 +0900

    Update script

--- app/mlr/script/run_gen_data.py
+++ app/mlr/script/run_gen_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from os.path import dirname
 from os.path import join
 
@@ -8,19 +9,23 @@ app_dir = dirname(dirname(os.path.realpath(__file__)))
 prog = join(app_dir, "bin", "gen_data_sparse")
 
 # no trailing /
-prefix_path = join(app_dir, "datasets")
+prefix_path = "/dfs/dataset/mlr"
+
+num_train = int(sys.argv[1])
+num_nodes = int(sys.argv[2])
+sparsity = 0.05 # 5 percent
 
 params = {
-    "num_train": 100
-    , "feature_dim": 10
-    , "num_partitions": 1
-    , "nnz_per_col": 10
+    "num_train": num_train
+    , "feature_dim": 20000
+    , "num_partitions": num_nodes
+    , "nnz_per_col": int(sparsity * num_train)
     , "one_based": True
-    , "beta_sparsity": 0.5
-    , "correlation_strength": 0.5
-    , "noise_ratio": 0.1
+    , "beta_sparsity": 1
+    , "correlation_strength": 0
+    , "noise_ratio": 0
     , "snappy_compressed": "false"
-    , "num_labels": 2
+    , "num_labels": 1000
     }
 params["output_file"] = join(prefix_path, "lr%d_dim%d_s%d_nnz%d") \
     % (params["num_labels"], params["feature_dim"], \
